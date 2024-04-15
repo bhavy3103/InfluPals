6c684bda62f17e685db0f61f2b74f3fa7ad7bc99
abhijivani3001 <abhijivani3001@gmail.com>
2024-04-15 15:09:51 +0530

    pricing pop-up done

--- frontend/page/profile.php
+++ frontend/page/profile.php
@@ -80,11 +80,11 @@
                     <div id="price-modal" class="hidden bg-white shadow-xl  border fixed rounded-xl left-[30%] top-[20%] w-[40vw] max-h-[60vh] z-30 overflow-y-hidden overflow-x-hidden flex justify-center items-center">
                         <div class="relative rounded-lg">
                             <!-- Modal header -->
-                            <div class="flex items-center justify-between py-2 px-4 md:p-5 border-b">
-                                <div class="text-xl text-center font-semibold text-gray-800">
+                            <div class="flex items-center justify-between py-2 px-4 md:p-5 border-b relative">
+                                <div class="text-3xl text-center mx-auto font-semibold text-gray-800">
                                     Pricing
                                 </div>
-                                <button onclick="showDialog()" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+                                <button onclick="showDialog()" type="button" class="absolute right-5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
                                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                     </svg>
