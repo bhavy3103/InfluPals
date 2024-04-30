de4cdd608b3ae61d6ac9ac1b24600aa3d9eb245a
abhijivani3001 <abhijivani3001@gmail.com>
2024-04-30 14:50:59 +0530

    category frequency

--- backend/api/getAllCategories.php
+++ backend/api/getAllCategories.php
@@ -3,17 +3,33 @@
 require_once '../../backend/db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $response = array();
+
     // SQL query to fetch categories
     $categoriesQuery = "SELECT * FROM category";
     $categoriesResult = mysqli_query($conn, $categoriesQuery);
 
-    if (!$categoriesResult) {
-        echo json_encode(array('error' => mysqli_error($conn))); // Return error message if query fails
+    // SQL query to fetch category frequencies
+    $categoryFreqQuery = "SELECT category, COUNT(*) as count FROM page GROUP BY category";
+    $categoryFreqResult = mysqli_query($conn, $categoryFreqQuery);
+
+    if (!$categoriesResult || !$categoryFreqResult) {
+        echo json_encode(array('errorrrr' => mysqli_error($conn))); // Return error message if query fails
         exit;
     }
 
-    $categories = mysqli_fetch_all($categoriesResult);
-    echo json_encode($categories); // Return response
+    // Fetch categories
+    $categories = mysqli_fetch_all($categoriesResult, MYSQLI_ASSOC);
+
+    // Fetch category frequencies
+    $categoryFreq = array();
+    while ($row = mysqli_fetch_assoc($categoryFreqResult)) {
+        $categoryFreq[$row['category']] = $row['count'];
+    }
+    $response['categoryFreq'] = $categoryFreq;
+
+    $response['categories'] = $categories;
+    echo json_encode($response); // Return response
 } else {
     http_response_code(405);
     echo "405 Method Not Allowed";
--- frontend/page/home.php
+++ frontend/page/home.php
@@ -46,23 +46,26 @@ if ( !(isset($_SESSION['id']) )) {
 
                 // Access and display limited data for each user
                 const categoriesGrid = document.getElementById('categoryGrid');
-                data.forEach(category => {
+                const categories=data.categories;
+                const categoryFreq=data.categoryFreq;
+
+                categories.forEach(category => {
                     const card = document.createElement('div');
                     card.classList.add('grid', 'grid-cols-5', 'gap-4', 'mb-6');
 
-                    card.addEventListener('click', () => openCategoryCards(category[1]));
+                    card.addEventListener('click', () => openCategoryCards(category.name));
 
                     card.innerHTML = `
-                    <a href="./cards.php?category=${category[1]}">
+                    <a href="./cards.php?category=${category.name}">
                         <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-xl bg-clip-border rounded-xl w-72">
                             <div class="relative h-40 mx-4 mt-6 overflow-hidden text-white shadow-xl bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-                                <img src="${category[2]}" alt="card-image" />
+                                <img src="${category.image_url}" alt="card-image" />
                             </div>
-                            <div class="p-3">
-                                <h5 class="block mt-2 text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 md:text-center">
-                                    ${category[1].toUpperCase()}
+                            <div class="flex justify-between py-3 px-10 items-center">
+                                <h5 class="block text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 md:text-center">
+                                    ${category.name?.toUpperCase()}
                                 </h5>
-
+                                <h5 class="text-xl text-gray-500">${categoryFreq[category.name] || 0}</h5>
                             </div>
                         </div>
                         </a>`;
